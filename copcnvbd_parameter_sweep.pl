b8dd0b28a76e5cfe40107c9b9484af8528ab1370
YYL-XD <liyaoyao@xidian.edu.cn>
2025-01-17 15:50:05 +0800

    Update copcnvbd_parameter_sweep.pl

--- copcnvbd_parameter_sweep.pl
+++ copcnvbd_parameter_sweep.pl
@@ -9,26 +9,26 @@ my $python_file2 = "copcnvbd_main.py";
 my $python_file3 = "sta_boundary.py";
 
 # input param of copodcnv
-my $param1_file1 = "/home/lyy/PycharmProjects/pythonProjecttest/CNV_data/simu_chr21_0.2_4x/";
+my $param1_file1 = "/../CNV_data/simu_chr21_0.2_4x/";
 my $param2_file1 = "/sim1_4_4100_read.sort.bam";
-my $param3_file1 = "/home/lyy/PycharmProjects/pythonProjecttest/CNV_data";
-my $param4_file1 = "/home/lyy/PycharmProjects/pythonProjecttest/CNV_data/simu_chr21_0.2_4x/result/COPOD_step1";
-my $param5_file1 = "/home/lyy/PycharmProjects/pythonProjecttest/CNV_data/simu_chr21_0.2_4x/result/COPOD_step1/seg_sim1";
+my $param3_file1 = "/../CNV_data";
+my $param4_file1 = "/../CNV_data/simu_chr21_0.2_4x/result/COPOD_step1";
+my $param5_file1 = "/../CNV_data/simu_chr21_0.2_4x/result/COPOD_step1/seg_sim1";
 my $param6_file1 = 1000;
 my $param7_file1 = 50;
 
 # input param of copodcnvbd
-my $param1_file2 = "/home/lyy/PycharmProjects/pythonProjecttest/CNV_data/simu_chr21_0.2_4x/";
+my $param1_file2 = "/../CNV_data/simu_chr21_0.2_4x/";
 my $param2_file2 = "/sim1_4_4100_read.sort.bam";
-my $param3_file2 = "/home/lyy/PycharmProjects/pythonProjecttest/CNV_data/";
-my $param4_file2 = "/home/lyy/PycharmProjects/pythonProjecttest/CNV_data/simu_chr21_0.2_4x/result/COPOD_step2/";
-my $param5_file2 = "/home/lyy/PycharmProjects/pythonProjecttest/CNV_data/simu_chr21_0.2_4x/result/COPOD_step1/";
+my $param3_file2 = "/../CNV_data/";
+my $param4_file2 = "/../CNV_data/simu_chr21_0.2_4x/result/COPOD_step2/";
+my $param5_file2 = "/../CNV_data/simu_chr21_0.2_4x/result/COPOD_step1/";
 my @param6_values_file2 = (0.2, 0.25, 0.3, 0.35);  
 #my $param6_file2 = 0.25;  
 
 
 # input param of statistic result
-#my $param1_file3  = "/home/lyy/PycharmProjects/pythonProjecttest/CNV_data/simu_chr21_0.2_4x/result/COPOD_step2/sim1_4_4100_read.sort.bam_step2_result_". $param6_file2 ."alpha.txt";
+# my $param1_file3  = "/../CNV_data/simu_chr21_0.2_4x/result/COPOD_step2/sim1_4_4100_read.sort.bam_step2_result_". $param6_file2 ."alpha.txt";
 my $param2_file3 = "groundtruth";
 
 
@@ -39,7 +39,7 @@ system("python3 $python_file1 $param1_file1 $param2_file1 $param3_file1 $param4_
 foreach my $param6 (@param6_values_file2) {
     print "Running $python_file2 with parameters: $param1_file2 $param2_file2 $param3_file2 $param4_file2 $param5_file2 $param6 \n";
     system("python3 $python_file2 $param1_file2 $param2_file2 $param3_file2 $param4_file2 $param5_file2 $param6") == 0 or die "Failed #to run $python_file2 with param6=$param6: $!";
-    my $param1_file3  = "/home/lyy/PycharmProjects/pythonProjecttest/CNV_data/simu_chr21_0.2_4x/result/COPOD_step2/sim1_4_4100_read.sort.bam_step2_result_". $param6 ."alpha.txt";
+    my $param1_file3  = "/../CNV_data/simu_chr21_0.2_4x/result/COPOD_step2/sim1_4_4100_read.sort.bam_step2_result_". $param6 ."alpha.txt";
     system("python3 $python_file3 $param1_file3 $param2_file3") == 0 or die "Failed #to run $python_file3: $!";
 }
 
